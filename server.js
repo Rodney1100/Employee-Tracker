237c454f5d1adfa3e8368f360307592abc6c50da
Rodney P <rodney.plancher@gmail.com>
2022-04-05 14:10:40 -0400

    connect to db

--- server.js
+++ server.js
@@ -1,16 +1,30 @@
 const express = require('express');
 const PORT = process.env.PORT || 3001;
+const mysql = require('mysql2')
 const app = express();
+
+// require('dotenv').config();
 
 // Express middleware
 app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
 
+// connect to db
+const db = mysql.createConnection(
+  {
+    host: "localhost",
+    user: "root",
+    password:"1100",
+    // password: process.env.DB_PW,
+    database: "emp_tracker",
+  },
+  console.log("Connected to the emp_tracker")
+)
 // default response for "not found" 
-app.use((req,res)=>{
-res.status(400).end()
+app.use((req, res) => {
+  res.status(400).end()
 })
 
-app.listen(PORT,()=>{
+app.listen(PORT, () => {
   console.log(`Server running on prt ${PORT}`)
 })
